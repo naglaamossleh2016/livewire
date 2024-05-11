023d7bc3b8d584579ecbfd78fbdd7e1dc43a592e
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2024-05-11 15:15:40 +0300

    validation form

---
+++ app/Livewire/ValidationComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ValidationComponent extends Component
+{
+    public $email;
+    public $password;
+    public $remember;
+    
+
+    public function submit(){
+         $this->validate([
+        'email' => 'required|email',
+        'password' => 'required|min:8', // Assuming a minimum password length of 8 characters
+        'remember' => 'nullable',
+    ]);
+        dd('from submit ');
+    }
+    public function render()
+    {
+        return view('livewire.validation-component');
+    }
+}
